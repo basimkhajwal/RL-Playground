dbde0c6f6a779b0403f6c56f5c8f59cfb0f073f8
Basim Khajwal <basimkhajwal@gmail.com>
2017-10-16 22:28:27 +0100

    Cleaned up interface, only dense layers will be used

--- shared/src/main/scala/rlp/ai/NeuralNetwork.scala
+++ shared/src/main/scala/rlp/ai/NeuralNetwork.scala
@@ -1,26 +1,29 @@
 package rlp.ai
 
-class NeuralNetwork {
+class NeuralNetwork(
+  val layerSizes: List[Int],
+  val activationFunctions: List[ActivationFunction],
+  val useSoftMax: Boolean = false
+) {
 
-}
+  assert(layerSizes.length >= 2, "Neural network requires input and output layer")
+  assert(layerSizes.length-1 == activationFunctions.length, "Invalid number of activation functions provided")
 
-trait NetworkLayer {
-  def forward(activations: Array[Double]): Array[Double]
-  def backward(deltas: Array[Double]): Array[Double]
+  
 }
 
 sealed trait ActivationFunction {
   def apply(x: Double): Double
   def derivative(x: Double): Double
 }
+
+object Linear extends ActivationFunction {
+  override def apply(x: Double): Double = x
+  override def derivative(x: Double): Double = 1
+}
+
 object ReLU extends ActivationFunction {
   override def apply(x: Double) = if (x < 0) 0 else x
   override def derivative(x: Double) = if (x < 0) 0 else 1
 }
 
-class DenseLayer(val activationFunction: ActivationFunction) extends NetworkLayer {
-
-  override def forward(activations: Array[Double]) = ???
-
-  override def backward(deltas: Array[Double]) = ???
-}
