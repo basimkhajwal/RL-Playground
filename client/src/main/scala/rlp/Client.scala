e790a272daed1f0394ac2eb614376616ca68440d
Basim Khajwal <basimkhajwal@gmail.com>
2017-11-09 23:29:31 +0000

    Added materialise initialiser

--- client/src/main/scala/rlp/Client.scala
+++ client/src/main/scala/rlp/Client.scala
@@ -4,11 +4,17 @@ import com.thoughtworks.binding.dom
 import org.scalajs.dom.document
 import rlp.pages.PongPage
 
+import scala.scalajs.js
+
 
 object Client {
 
   def main(args: Array[String]): Unit = {
     val page = new PongPage()
     dom.render(document.getElementById("clientContainer"), page.content)
+
+    /* Run Jquery to initialise materialize */
+    val $ = js.Dynamic.global.$
+    $("select").material_select()
   }
 }
