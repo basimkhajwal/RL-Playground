1ee1f2065abaeb208ff04d3ecfa78a476586441c
Basim Khajwal <basimkhajwal@gmail.com>
2017-11-12 15:22:01 +0000

    Added a nicer state space mapping DSL and used it to start with a pong q table model controller

--- client/src/main/scala/rlp/controllers/QTableController.scala
+++ client/src/main/scala/rlp/controllers/QTableController.scala
@@ -6,11 +6,25 @@ import rlp.ai.agents.QTableAgent
 import rlp.environment.{Agent, MappedAgent}
 
 class QTableController[O, A](
-  numStates: Int, stateMap: (O) => Int,
   numActions: Int, actionMap: (Int) => A,
+  spaces: QStateSpace[O]*
 ) extends ModelController[Agent[O, A]] {
 
   override val name: String = "Tabular Q Learning"
+
+  val numStates = spaces.map(_.size).sum
+
+  private def stateMap(state: O): Int ={
+    var stateSize = 1
+    var currentIdx = 0
+
+    for (space <- spaces) {
+      currentIdx += stateSize * space(state)
+      stateSize *= space.size
+    }
+
+    currentIdx
+  }
 
   @dom
   override lazy val modelOptions: Binding[Div] = {
@@ -23,3 +37,21 @@ class QTableController[O, A](
     new MappedAgent(new QTableAgent(numStates, numActions), stateMap, actionMap)
   }
 }
+
+class QStateSpace[T](val name: String, val size: Int, val map: T => Int) {
+  def apply(s: T): Int = map(s)
+}
+
+object QStateSpace {
+
+  def discrete[T](name: String, n: Int, map: (T) => Int) = new QStateSpace(name, n, map)
+
+  def boxed[T](
+    name: String, low: Double, high: Double, divisions: Int = 10,
+    map: T => Double
+  ) = {
+    discrete[T](name, divisions, { s => (divisions * (map(s) - low) / (high-low)).toInt })
+  }
+}
+
+
--- client/src/main/scala/rlp/pages/PongPage.scala
+++ client/src/main/scala/rlp/pages/PongPage.scala
@@ -3,17 +3,28 @@ package rlp.pages
 
 import com.thoughtworks.binding.Binding.Var
 import rlp._
-import rlp.environment.{Environment, NaivePongAgent, Pong}
+import rlp.environment.{Agent, Environment, NaivePongAgent, Pong}
 import com.thoughtworks.binding.{Binding, dom}
 import org.scalajs.dom.{CanvasRenderingContext2D, document, html}
 import org.scalajs.dom.html.{Canvas, Div}
 import org.scalajs.dom.raw.Event
-import rlp.controllers.{ModelController, PongQNetworkController, PongQTableController}
+import rlp.controllers.{ModelController, QTableController, QStateSpace}
 
 import scala.scalajs.js.{Date, timers}
 
-class PongPage extends GamePage[Pong.State, Pong.AgentState, Pong.Action] {
+class PongPage extends GamePage[Agent[Pong.AgentState, Pong.Action]] {
 
-  override val modelControllers = new PongQNetworkController() :: new PongQTableController() :: Nil
+  import Pong._
+  import QStateSpace._
+
+  override val modelControllers = List(
+    new QTableController(
+      2, { a => if (a == 0) UpAction else DownAction },
+      boxed[AgentState]("Ball X", 0, SCREEN_WIDTH, 10, _.ballPos.x),
+      boxed[AgentState]("Ball Y", 0, SCREEN_HEIGHT, 10, _.ballPos.y),
+      boxed[AgentState]("Paddle Y", 0, SCREEN_HEIGHT, 10, _.currentPaddle)
+    )
+  )
+
 }
 
