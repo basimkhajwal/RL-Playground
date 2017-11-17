45988b10a5ef97fd8a0a1bfa8eb233d1043b60a0
Basim Khajwal <basimkhajwal@gmail.com>
2017-11-17 19:27:20 +0000

    Styled a bit, looks a lot neater

--- client/src/main/scala/rlp/pages/GamePage.scala
+++ client/src/main/scala/rlp/pages/GamePage.scala
@@ -118,7 +118,7 @@ abstract class GamePage[A] {
   }
 
   protected def pageResized(): Unit = {
-    val container = document.getElementById("canvasContainer").asInstanceOf[Div]
+    val container = document.getElementById("canvas-container").asInstanceOf[Div]
     val width = Math.min(800, container.offsetWidth - 50)
     val aspectRatio = (canvas.height * 1.0) / canvas.width
 
@@ -131,7 +131,7 @@ abstract class GamePage[A] {
 
     <div id="app">
 
-      <nav class="orange z-depth-0">
+      <nav class="teal z-depth-0">
         <div class="nav-wrapper page-container">
           <a href="#" class="brand-logo left">RL-Playground</a>
           <p id="subtitle" class="right hide-on-med-and-down">
@@ -146,17 +146,34 @@ abstract class GamePage[A] {
           <h5 class="center-align">Pong</h5>
         </div>
 
-        <div class="col s8">
-          { gameContainer.bind }
-        </div>
-        <div class="col s4">
-          { controlsSection.bind }
+        <div class="col s12">
+          <div class="card">
+            <div class="row">
+              <div class="col s8">
+                { gameContainer.bind }
+              </div>
+              <div class="col s4 teal lighten-5">
+                { controlsSection.bind }
+              </div>
+            </div>
+          </div>
         </div>
 
         <div class="col s12">
-          <div class="card-panel">
-            <h5 class="center-align">Model Hyper Parameters</h5>
-            { modelControllers(modelIdx.bind).modelOptions.bind }
+          <div class="card" id="model-select">
+            <div class="card-content">
+              <div class="row">
+                <div class="col s6">
+                  <span class="card-title">Model</span>
+                </div>
+                <div class="col s4 offset-s1">
+                  { modelSelection.bind }
+                </div>
+              </div>
+            </div>
+            <div class="card-content blue-grey lighten-5">
+              { modelControllers(modelIdx.bind).modelOptions.bind }
+            </div>
           </div>
         </div>
 
@@ -173,11 +190,11 @@ abstract class GamePage[A] {
 
   @dom
   protected lazy val controlsSection: Binding[Div] = {
-    <div class="card-panel">
-      <h5 class="center-align">Controls</h5>
+    <div id="control-section">
+      <span class="card-title">Training Controls</span>
       { trainingButtons.bind } <br />
 
-      <h6>
+      <h6 class="center-align">
         {
         if (trainState.bind == Playing) {
           s"Speed: ${GAME_SPEED_VALUES(gameSpeed.bind)}"
@@ -187,13 +204,12 @@ abstract class GamePage[A] {
         }
       </h6> <br />
 
-      { modelSelection.bind }
-
       <br />
-      <h5>Game Options</h5>
+      <span class="card-title">Game Options</span>
+      <br />
       <br />
 
-      <div class="switch">
+      <div class="switch center-align">
         <label>
           Play Game
           <input type="checkbox" checked={renderTraining.bind} onchange={ _:Event => toggleRenderTraining() } />
@@ -215,35 +231,37 @@ abstract class GamePage[A] {
 
   @dom
   protected lazy val trainingButtons: Binding[Div] = {
-    val buttonStyle = "btn-floating waves-effect waves-circle "
+    val buttonStyle = "center-align btn-floating waves-effect waves-circle "
     val state = trainState.bind
 
-    <div class="valign-wrapper center-align">
-      <a class= {
-         buttonStyle + "btn-medium orange right " +
-           (if (state == Stopped) "disabled" else "")
-         }
-         onclick={ _:Event => stopClicked() }
-      >
-        <i class="material-icons">stop</i>
-      </a>
+    <div class="center-align" id="buttonsContainer">
+      <div class="valign-wrapper">
+        <a class= {
+           buttonStyle + "btn-medium orange " +
+             (if (state == Stopped) "disabled" else "")
+           }
+           onclick={ _:Event => stopClicked() }
+        >
+          <i class="material-icons">stop</i>
+        </a>
 
-      <a class={buttonStyle + "btn-large red"}
-         onclick = { _:Event => if (state == Playing) pauseClicked() else playClicked() }
-      >
-        <i class="material-icons">
-          { if (state == Playing) "pause" else "play_arrow" }
-        </i>
-      </a>
+        <a class={buttonStyle + "btn-large red"}
+           onclick = { _:Event => if (state == Playing) pauseClicked() else playClicked() }
+        >
+          <i class="material-icons">
+            { if (state == Playing) "pause" else "play_arrow" }
+          </i>
+        </a>
 
-      <a class= {
-         buttonStyle + "btn-medium orange left " +
-           (if (state != Playing) "disabled" else "")
-         }
-         onclick={ _:Event => fastForwardClicked() }
-      >
-        <i class="material-icons">fast_forward</i>
-      </a>
+        <a class= {
+           buttonStyle + "btn-medium orange " +
+             (if (state != Playing) "disabled" else "")
+           }
+           onclick={ _:Event => fastForwardClicked() }
+        >
+          <i class="material-icons">fast_forward</i>
+        </a>
+      </div>
     </div>
   }
 
@@ -265,11 +283,8 @@ abstract class GamePage[A] {
   @dom
   protected lazy val gameContainer: Binding[Div] = {
     canvas = <canvas class="center-align" width={800} height={600}></canvas>
-    keyboardHandler.register(canvas)
     ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
 
-    <div class="card-panel" id="canvasContainer">
-      { canvas }
-    </div>
+    <div id="canvas-container" class="center-align valign-wrapper"> { canvas } </div>
   }
 }
