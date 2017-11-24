683caa920b3cfe8d12a336fa9a61f94115d3fcea
basimkhajwal <basimkhajwal@gmail.com>
2017-11-24 12:16:52 +0000

    Began adding optimization library and started with a simple SGB implementation with a test showing improvement over the classical implementation of gradient descent

---
+++ shared/src/main/scala/rlp/ai/optimizers/NetworkOptimizer.scala
@@ -0,0 +1,68 @@
+package rlp.ai.optimizers
+
+import rlp.ai.ActivationFunction.{ReLU, Sigmoid}
+import rlp.ai.NeuralNetwork
+import rlp.math.Matrix
+
+abstract class NetworkOptimizer(val network: NeuralNetwork) {
+
+  def step(input: Matrix, target: Matrix): Unit = {
+    val gradients = network.backProp(input, target)
+    step(gradients)
+  }
+
+  def step(gradient: Array[Matrix]): Unit
+}
+
+class SGDMomentum(network: NeuralNetwork,
+                   learningRate: Double,
+                    momentumRate: Double
+                  ) extends NetworkOptimizer(network) {
+
+  val velocity: Array[Matrix] = network.weights.map(w => new Matrix(w.getRows, w.getCols))
+  val numWeights = velocity.length
+
+  override def step(gradient: Array[Matrix]): Unit = {
+    for (i <- 0 until numWeights) {
+      velocity(i) *= momentumRate
+      velocity(i) -= (gradient(i) * learningRate)
+
+      network.weights(i) += velocity(i)
+    }
+  }
+}
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    val networkA, networkB = new NeuralNetwork(Array(2, 5, 1), Array(Sigmoid, ReLU))
+
+    networkA.randomiseWeights()
+    for (i <- networkA.weights.indices) networkB.weights(i) = new Matrix(networkA.weights(i))
+
+    val learningRate = 0.1
+    val momentumRate = 0.7
+
+    val sgd = new SGDMomentum(networkA, learningRate, 0)
+    val sgdMomentum = new SGDMomentum(networkB, learningRate, momentumRate)
+
+    val dataSamples = 200
+    val function = (x:Double, y:Double) => 4*x+2*y
+
+    val inputData = new Matrix(dataSamples, 2) each (_ => math.random())
+    val targets = new Matrix(dataSamples, 1,
+      (0 until dataSamples) map (i => function(inputData(i,0),inputData(i,1))) toArray)
+
+    println("Epoch\t\tSGD\t\t\t\tSGD+Momentum")
+    for (i <- 0 until 200) {
+      sgd.step(inputData, targets)
+      sgdMomentum.step(inputData, targets)
+
+      if (i % 20 == 0) {
+        val lossA = networkA.loss(inputData, targets).sum
+        val lossB = networkB.loss(inputData, targets).sum
+        println(s"$i\t\t$lossA\t\t$lossB")
+      }
+    }
+  }
+}
