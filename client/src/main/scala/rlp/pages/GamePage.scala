fb8dedb2780cf24733473214dfac82cb72cd7b8b
Basim Khajwal <basimkhajwal@gmail.com>
2017-11-26 13:31:31 +0000

    Refactored training code into separate class

--- client/src/main/scala/rlp/pages/GamePage.scala
+++ client/src/main/scala/rlp/pages/GamePage.scala
@@ -15,9 +15,6 @@ abstract class GamePage[A] {
 
   protected val targetGameWidth = 800
 
-  protected val gameSpeedMultiplier = List(1, 2, 5, 10, -1)
-  protected val gameSpeedToString = gameSpeedMultiplier.init.map("x"+_) ++ List("Max")
-
   /* Abstract vars */
   protected val modelControllerBuilders: List[ModelController.Builder[A]]
 
@@ -27,38 +24,17 @@ abstract class GamePage[A] {
   protected def trainStep(): Unit
   protected def render(ctx: CanvasRenderingContext2D): Unit
 
-  protected val isTraining: Var[Boolean] = Var(false)
-  protected val gameSpeed: Var[Int] = Var(0)
   protected val renderTraining: Var[Boolean] = Var(true)
 
   protected val models: Vars[Model[A]] = Vars()
 
-  val modelSelect = new SelectHandler(
-    "Model Select",
-    models.map(m => m.controller.name + " - " + m.name),
-    Constant(false)
-  )
-
-  val modelExists = Binding { models.bind.nonEmpty }
-
-  val selectedModel: Binding[Option[Model[A]]] = Binding {
-    if (modelExists.bind) {
-      val model = models.bind(modelSelect.selectedIndex.bind)
-      modelChanged(model)
-
-      Some(model)
-    } else {
-      None
-    }
-  }
-
   lazy val modelBuilder = new ModelBuilder(modelControllerBuilders, models)
+  lazy val modelTrainer = new ModelTrainer(models, modelBuilder, trainStep)
 
   private var canvas: Canvas = _
   private var ctx: CanvasRenderingContext2D = _
   protected val keyboardHandler = new KeyboardHandler()
 
-  private val trainingProcess = new BackgroundProcess(trainStep)
   private val renderProcess = new BackgroundProcess(() => render(ctx))
 
   def start(): Unit = {
@@ -67,43 +43,12 @@ abstract class GamePage[A] {
     pageResized()
   }
 
-  protected def modelChanged(model: Model[A]): Unit = {
-    if (isTraining.get) pauseTraining()
-    initModel(model)
-
-    js.timers.setTimeout(100) { js.Dynamic.global.Materialize.updateTextFields() }
-  }
-
-  protected def startTraining(): Unit = {
-    trainingProcess.start(Environment.FPS * gameSpeedMultiplier(gameSpeed.get))
-    isTraining := true
-  }
-
-  protected def pauseTraining(): Unit = {
-    trainingProcess.stop()
-    isTraining := false
-  }
-
-  @dom
-  protected def resetTraining(): Unit = {
-    val model = selectedModel.bind.get
-
-    model.controller.resetAgent()
-    model.gamesPlayed := 0
-  }
-
-  protected def fastForwardTraining(): Unit = {
-    gameSpeed := (gameSpeed.get + 1) % gameSpeedMultiplier.length
-    trainingProcess.stop()
-    trainingProcess.start(Environment.FPS * gameSpeedMultiplier(gameSpeed.get))
-  }
-
   protected def toggleRenderTraining(): Unit = {
     renderTraining := !renderTraining.get
   }
 
   protected def pageResized(): Unit = {
-    val container = document.getElementById("canvas-container").asInstanceOf[Div]
+    val container = getElem[Div]("canvas-container")
     val width = Math.min(targetGameWidth, container.offsetWidth - 50)
 
     canvas.width = width.toInt
@@ -146,7 +91,13 @@ abstract class GamePage[A] {
         <div class="col s12">
           <div class="card" id="model-select">
             <div class="card-content">
-              { modelViewSection.bind }
+              {
+                modelTrainer.selectedModel.bind match {
+                  case Some(model) => initModel(model)
+                  case None => /* Do nothing */
+                }
+                modelTrainer.content.bind
+              }
             </div>
 
             <div class="card-reveal">
@@ -161,52 +112,8 @@ abstract class GamePage[A] {
   }
 
   @dom
-  protected lazy val modelViewSection: Binding[Div] = {
-
-    <div class="row">
-
-      <div class="row grey col s12 lighten-3" id="model-training">
-        <div class="col s3">
-          <span class="card-title">Model Training</span>
-        </div>
-
-        <div class="col s3">
-          { modelSelect.handler.bind }
-        </div>
-
-        <div class="col s6" id="model-training-btns">
-          {
-            val btnStyle = "btn waves-effect waves-light"
-
-            <!-- TODO: IMPLEMENT FUNCTIONALITY FOR THESE  -->
-            <a class={btnStyle + " disabled"}>Clone</a>
-            <a class={btnStyle + " disabled"}>Duplicate</a>
-            <a class={btnStyle + " activator"}>New</a>
-          }
-          <!--<a class="btn-floating btn waves-effect waves-light red activator right"><i class="material-icons">add</i></a>-->
-        </div>
-      </div>
-
-      <div class="col s12">
-        { if (modelExists.bind) trainingControls.bind else <!-- --> }
-      </div>
-
-      <div class="col s12">
-        {
-          selectedModel.bind match {
-            case None => <!-- -->
-            case Some(model) => model.controller.modelViewer.bind
-          }
-        }
-      </div>
-
-    </div>
-  }
-
-  @dom
   protected lazy val controlsSection: Binding[Div] = {
     <div id="control-section">
-      <br />
       <span class="card-title">Game Options</span>
       <br />
 
@@ -224,63 +131,8 @@ abstract class GamePage[A] {
   }
 
   @dom
-  protected lazy val trainingControls: Binding[Div] = {
-    <div class="row">
-      <div class="col s6">
-        { trainingButtons.bind }
-      </div>
-
-      <div class="col s6 valign-wrapper">
-        <h6 class="center-align">
-          {
-          selectedModel.bind match {
-            case Some(model) => s"Games Played: ${model.gamesPlayed.bind}"
-            case None => ""
-          }
-          }
-        </h6>
-      </div>
-    </div>
-  }
-
-  @dom
   protected lazy val gameOptions: Binding[Div] = {
-    <div>
-      Empty!
-    </div>
-  }
-
-  @dom
-  protected lazy val trainingButtons: Binding[Div] = {
-    val buttonStyle = "center-align btn-floating waves-effect waves-circle "
-    val training = isTraining.bind
-
-    <div class="center-align" id="buttons-container">
-      <div class="valign-wrapper">
-        <a class= {buttonStyle + "btn-medium orange"}
-           onclick={ _:Event => resetTraining() }
-        >
-          <i class="material-icons">replay</i>
-        </a>
-
-        <a class={buttonStyle + "btn-large red"}
-           onclick = { _:Event => if (training) pauseTraining() else startTraining() }
-        >
-          <i class="material-icons">
-            { if (training) "pause" else "play_arrow" }
-          </i>
-        </a>
-
-        <a class= {
-             buttonStyle + "btn-medium orange " + (if (training) "" else "disabled")
-           }
-           onclick={ _:Event => fastForwardTraining() }
-        >
-          <i class="material-icons">fast_forward</i>
-        </a>
-        <span id="training-speed"> { if (isTraining.bind) gameSpeedToString(gameSpeed.bind) else "" } </span>
-      </div>
-    </div>
+    <div>Empty!</div>
   }
 
   @dom
---
+++ client/src/main/scala/rlp/pages/ModelTrainer.scala
@@ -0,0 +1,168 @@
+package rlp.pages
+
+import com.thoughtworks.binding.{Binding, dom}
+import com.thoughtworks.binding.Binding.{Constant, Var, Vars}
+import org.scalajs.dom.Event
+import org.scalajs.dom.html.Div
+import rlp.environment.Environment
+import rlp.{BackgroundProcess, SelectHandler}
+
+import scala.scalajs.js
+
+class ModelTrainer[A](
+  models: Vars[Model[A]],
+  builder: ModelBuilder[A],
+  trainStep: () => Unit,
+) {
+
+  val gameSpeedMultiplier = List(1, 2, 5, 10, -1)
+  val gameSpeedToString = gameSpeedMultiplier.init.map("x"+_) ++ List("Max")
+
+  val isTraining: Var[Boolean] = Var(false)
+  val gameSpeed: Var[Int] = Var(0)
+
+  val modelSelect = new SelectHandler(
+    "Model Select",
+    models.map(m => m.controller.name + " - " + m.name),
+    Constant(false)
+  )
+
+  val modelExists = Binding { models.bind.nonEmpty }
+
+  val selectedModel: Binding[Option[Model[A]]] = Binding {
+    if (modelExists.bind) {
+      val model = models.bind(modelSelect.selectedIndex.bind)
+      modelSelected(model)
+      Some(model)
+    } else {
+      None
+    }
+  }
+
+  private val trainingProcess = new BackgroundProcess(trainStep)
+
+  private def startTraining(): Unit = {
+    trainingProcess.start(Environment.FPS * gameSpeedMultiplier(gameSpeed.get))
+    isTraining := true
+  }
+
+  private def pauseTraining(): Unit = {
+    trainingProcess.stop()
+    isTraining := false
+  }
+
+  private def modelSelected(model: Model[A]): Unit = {
+    if (isTraining.get) pauseTraining()
+    js.timers.setTimeout(100) { js.Dynamic.global.Materialize.updateTextFields() }
+  }
+
+  @dom
+  private def resetTraining(): Unit = {
+    val model = selectedModel.bind.get
+
+    model.controller.resetAgent()
+    model.gamesPlayed := 0
+  }
+
+  private def fastForwardTraining(): Unit = {
+    gameSpeed := (gameSpeed.get + 1) % gameSpeedMultiplier.length
+    trainingProcess.stop()
+    trainingProcess.start(Environment.FPS * gameSpeedMultiplier(gameSpeed.get))
+  }
+
+  @dom
+  lazy val content: Binding[Div] = {
+
+    <div class="row">
+
+      <div class="row grey col s12 lighten-3" id="model-training">
+        <div class="col s3">
+          <span class="card-title">Model Training</span>
+        </div>
+
+        <div class="col s3">
+          { modelSelect.handler.bind }
+        </div>
+
+        <div class="col s6" id="model-training-btns">
+          {
+          val btnStyle = "btn waves-effect waves-light"
+
+          <!-- TODO: IMPLEMENT FUNCTIONALITY FOR THESE  -->
+            <a class={btnStyle + " disabled"}>Clone</a>
+            <a class={btnStyle + " disabled"}>Duplicate</a>
+            <a class={btnStyle + " activator"}>New</a>
+          }
+          <!--<a class="btn-floating btn waves-effect waves-light red activator right"><i class="material-icons">add</i></a>-->
+        </div>
+      </div>
+
+      <div class="col s12">
+        { if (modelExists.bind) trainingControls.bind else <!-- --> }
+      </div>
+
+      <div class="col s12">
+        {
+        selectedModel.bind match {
+          case None => <!-- -->
+          case Some(model) => model.controller.modelViewer.bind
+        }
+        }
+      </div>
+
+    </div>
+  }
+
+  @dom
+  private lazy val trainingControls: Binding[Div] = {
+    <div class="row">
+      <div class="col s6">
+        { trainingButtons.bind }
+      </div>
+
+      <div class="col s6 valign-wrapper">
+        <h6 class="center-align">
+          {
+            selectedModel.bind match {
+              case Some(m) => s"Games Played: ${m.gamesPlayed.bind}"
+              case None => ""
+            }
+          }
+        </h6>
+      </div>
+    </div>
+  }
+
+  @dom
+  private lazy val trainingButtons: Binding[Div] = {
+    val buttonStyle = "center-align btn-floating waves-effect waves-circle "
+    val training = isTraining.bind
+
+    <div class="center-align" id="buttons-container">
+      <div class="valign-wrapper">
+        <a class= {buttonStyle + "btn-medium orange"}
+           onclick={ _:Event => resetTraining() }
+        >
+          <i class="material-icons">replay</i>
+        </a>
+
+        <a class={buttonStyle + "btn-large red"}
+           onclick = { _:Event => if (training) pauseTraining() else startTraining() }
+        >
+          <i class="material-icons">
+            { if (training) "pause" else "play_arrow" }
+          </i>
+        </a>
+
+        <a class= {
+           buttonStyle + "btn-medium orange " + (if (training) "" else "disabled")
+           }
+           onclick={ _:Event => fastForwardTraining() }
+        >
+          <i class="material-icons">fast_forward</i>
+        </a>
+        <span id="training-speed"> { if (isTraining.bind) gameSpeedToString(gameSpeed.bind) else "" } </span>
+      </div>
+    </div>
+  }
+}
