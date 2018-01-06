579f84484bdf960b1182e256a75c4ab3152afd24
Basim Khajwal <basimkhajwal@gmail.com>
2018-01-06 14:34:20 +0000

    Copied across views back into server subproject

--- server/app/rlp/controllers/Application.scala
+++ server/app/rlp/controllers/Application.scala
@@ -2,14 +2,14 @@ package rlp.controllers
 
 import javax.inject._
 
-import rlp.shared.SharedMessages
 import play.api.mvc._
 
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
-    Ok(views.html.index())
-  }
+  def index = Action { Ok(views.html.index()) }
 
+  def login = Action { Ok(views.html.login()) }
+
+  def signUp = Action { Ok(views.html.signup()) }
 }
