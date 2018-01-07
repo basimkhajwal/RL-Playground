ea0587d3f0234ec43f51df5475fe04a1a2c6fd85
Basim Khajwal <basimkhajwal@gmail.com>
2018-01-07 18:53:20 +0000

    Implemented basic controllers

--- server/app/rlp/controllers/Application.scala
+++ server/app/rlp/controllers/Application.scala
@@ -3,13 +3,28 @@ package rlp.controllers
 import javax.inject._
 
 import play.api.mvc._
+import rlp.forms.{LoginForm, SignUpForm}
 
 @Singleton
 class Application @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action { Ok(views.html.index()) }
+  def index = Action { implicit request => Ok(views.html.index()) }
 
-  def login = Action { Ok(views.html.login()) }
+  def login = Action { implicit request => Ok(views.html.login(LoginForm.form)) }
 
-  def signUp = Action { Ok(views.html.signup()) }
+  def loginPost = Action { implicit request =>
+    LoginForm.form.bindFromRequest.fold(
+      formError => BadRequest(views.html.login(formError)),
+      _ => Ok(views.html.index())
+    )
+  }
+
+  def signUp = Action { implicit request => Ok(views.html.signup(SignUpForm.form)) }
+
+  def signUpPost = Action { implicit request =>
+    SignUpForm.form.bindFromRequest.fold(
+      formError => BadRequest(views.html.signup(formError)),
+      _ => Ok(views.html.index())
+    )
+  }
 }
