0daf48cf4f22579354d1281327e13a80241398d1
Basim Khajwal <basimkhajwal@gmail.com>
2018-01-09 23:05:40 +0000

    Added initialiser for database schemas

---
+++ server/app/rlp/utils/InitDatabase.scala
@@ -0,0 +1,27 @@
+package rlp.utils
+
+import play.api._
+import rlp.dao.UserDAO
+
+object InitDatabase {
+
+  def loadApp(): Application = {
+    val env = Environment(new java.io.File("."), this.getClass.getClassLoader, Mode.Dev)
+    val context = ApplicationLoader.createContext(env)
+    val loader = ApplicationLoader(context)
+    val app = loader.load(context)
+    Play.start(app)
+
+    app
+  }
+
+  def main(): Unit = {
+    val app = loadApp()
+
+    /* Get schema instances */
+    val userDAO = app.injector.instanceOf[UserDAO]
+
+    // Init schema
+    userDAO.createSchema()
+  }
+}
