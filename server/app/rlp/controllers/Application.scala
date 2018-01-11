963adae9405393dd89b4895b4de79c49635ce122
Basim Khajwal <basimkhajwal@gmail.com>
2018-01-11 00:08:42 +0000

    Added login handling (it works :D)

--- server/app/rlp/controllers/Application.scala
+++ server/app/rlp/controllers/Application.scala
@@ -3,22 +3,39 @@ package rlp.controllers
 import javax.inject._
 
 import play.api.mvc._
+import rlp.actions.UserAction
 import rlp.forms.{LoginForm, SignUpForm}
+import rlp.models.User
 import rlp.service.UserService
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class Application @Inject()(userService: UserService, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+class Application @Inject()(userService: UserService, cc: ControllerComponents, userAction: UserAction)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
-  def index = Action { implicit request => Ok(views.html.index()) }
+  def index = userAction { implicit request =>
+    Ok(views.html.index(request.user))
+  }
+
+  def bindUserSession(user: User)(result: Result): Result = {
+    result.withSession("username" -> user.username)
+  }
 
   def login = Action { implicit request => Ok(views.html.login(LoginForm.form)) }
 
-  def loginPost = Action { implicit request =>
+  def loginPost = Action.async { implicit request =>
     LoginForm.form.bindFromRequest.fold(
-      formError => BadRequest(views.html.login(formError)),
-      _ => Ok(views.html.index())
+      formError => Future.successful { BadRequest(views.html.login(formError)) },
+      loginForm => {
+        userService.authenticate(loginForm).map {
+          case Some(user) => bindUserSession(user) { Redirect(routes.Application.index()) }
+          case _ => {
+            BadRequest(views.html.login(
+              LoginForm.form.fill(loginForm) withGlobalError "Invalid username or password"
+            ))
+          }
+        }
+      }
     )
   }
 
@@ -28,9 +45,9 @@ class Application @Inject()(userService: UserService, cc: ControllerComponents)(
     SignUpForm.form.bindFromRequest.fold(
       formError => Future.successful(BadRequest(views.html.signup(formError))),
       signUpForm => {
-        for {
-          _ <- userService.createUser(signUpForm)
-        } yield Ok(views.html.index())
+        userService.createUser(signUpForm) map { user =>
+          bindUserSession(user) { Redirect(routes.Application.index()) }
+        }
       }
     )
   }
