675ffce1e9700bab025b79445bcedf19ef696039
Basim Khajwal <basimkhajwal@gmail.com>
2018-01-15 22:47:28 +0000

    Started idb stuff

--- client/src/main/scala/rlp/dao/LocalModelDAO.scala
+++ client/src/main/scala/rlp/dao/LocalModelDAO.scala
@@ -1,10 +1,10 @@
 package rlp.dao
-import rlp.models.Model
-import org.scalajs.dom.{html, window}
-import org.scalajs.dom.idb.{Database, OpenDBRequest}
+import org.scalajs.dom.{ErrorEvent, Event, html, window}
+import org.scalajs.dom.idb.{Database, Request}
 import rlp.storage.ModelStore
 
 import scala.concurrent.{Future, Promise}
+import scala.scalajs.js
 
 object LocalModelDAO extends ModelDAO {
 
@@ -13,29 +13,46 @@ object LocalModelDAO extends ModelDAO {
   private def getDatabase(): Future[Database] = {
 
     val dBRequest = window.indexedDB.open("rlp", 1)
-    val promise = Promise[Database]()
 
     dBRequest.onupgradeneeded = { event =>
       val db = dBRequest.result.asInstanceOf[Database]
 
-      val objectStore = db.createObjectStore("modelStore")
+      val modelStore = db.createObjectStore("modelStore", js.Dynamic.literal("keyPath" -> "id"))
 
-
+      val failSafe = db.createObjectStore("failSafe", js.Dynamic.literal("keyPath" -> "id"))
+      failSafe.createIndex("uid", "uid", js.Dynamic.literal("unique" -> false))
     }
 
-    dBRequest.onerror = { err =>
-      promise.failure(new Exception(err.message))
+    requestPromise(dBRequest) { _ =>
+      dBRequest.result.asInstanceOf[Database]
+    }
+  }
+
+  def requestPromise[A](request: Request)(success: Event => A): Future[A] = {
+    requestPromise(request, success, err => new Exception(err.message))
+  }
+
+  def requestPromise[A](request: Request, success: Event => A, error: ErrorEvent => Throwable): Future[A] = {
+    val promise = Promise[A]()
+
+    request.onsuccess = { event =>
+      promise.success(success(event))
     }
 
-    dBRequest.onsuccess = { _ =>
-      promise.success(dBRequest.result.asInstanceOf[Database])
+    request.onerror = { event =>
+      promise.failure(error(event))
     }
 
     promise.future
   }
 
   override def getAll[A](): Future[Seq[ModelStore]] = {
-    Future { List() }
+    // TODO: Complete cursor to get model store
+    for {
+      db <- getDatabase()
+    } yield {
+     List()
+    }
   }
 
   override def persist(model: ModelStore): Future[Boolean] = {
