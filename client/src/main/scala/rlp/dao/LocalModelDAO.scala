7521c244b216f3df598cc506b546fada229809d5
Basim Khajwal <basimkhajwal@gmail.com>
2018-01-16 22:48:05 +0000

    Implemented CRUD methods using IDB handler

--- client/src/main/scala/rlp/dao/LocalModelDAO.scala
+++ client/src/main/scala/rlp/dao/LocalModelDAO.scala
@@ -1,65 +1,64 @@
 package rlp.dao
-import org.scalajs.dom.{ErrorEvent, Event, html, window}
-import org.scalajs.dom.idb.{Database, Request}
 import rlp.storage.ModelStore
+import upickle.default
 
-import scala.concurrent.{Future, Promise}
+import scala.concurrent.Future
 import scala.scalajs.js
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.scalajs.js.annotation.{JSExportTopLevel, ScalaJSDefined}
 
 object LocalModelDAO extends ModelDAO {
 
-  private val DB_NAME = "rlp"
-
-  private def getDatabase(): Future[Database] = {
-
-    val dBRequest = window.indexedDB.open("rlp", 1)
-
-    dBRequest.onupgradeneeded = { event =>
-      val db = dBRequest.result.asInstanceOf[Database]
-
-      val modelStore = db.createObjectStore("modelStore", js.Dynamic.literal("keyPath" -> "id"))
-
-      val failSafe = db.createObjectStore("failSafe", js.Dynamic.literal("keyPath" -> "id"))
-      failSafe.createIndex("uid", "uid", js.Dynamic.literal("unique" -> false))
-    }
-
-    requestPromise(dBRequest) { _ =>
-      dBRequest.result.asInstanceOf[Database]
-    }
-  }
-
-  def requestPromise[A](request: Request)(success: Event => A): Future[A] = {
-    requestPromise(request, success, err => new Exception(err.message))
-  }
-
-  def requestPromise[A](request: Request, success: Event => A, error: ErrorEvent => Throwable): Future[A] = {
-    val promise = Promise[A]()
-
-    request.onsuccess = { event =>
-      promise.success(success(event))
-    }
-
-    request.onerror = { event =>
-      promise.failure(error(event))
-    }
-
-    promise.future
-  }
-
   override def getAll[A](): Future[Seq[ModelStore]] = {
-    // TODO: Complete cursor to get model store
-    for {
-      db <- getDatabase()
-    } yield {
-     List()
+    IndexedDB.getAll[ModelStoreItem](IndexedDB.MODEL_STORE) map { items =>
+      items map { item => ModelStoreItem.extract(item) }
     }
   }
 
-  override def update(model: ModelStore): Future[Boolean] = {
-    Future(false)
+  override def create(model: ModelStore): Future[Long] = {
+
+    val modelID: Long = System.currentTimeMillis()
+    val item = ModelStoreItem.fromStore(model)
+    item.id = modelID
+
+    IndexedDB
+      .create(IndexedDB.MODEL_STORE, item)
+      .map { _ => modelID }
   }
 
-  override def delete(model: ModelStore): Future[Boolean] = {
-    Future(false)
+  override def update(model: ModelStore): Future[Unit] = {
+    IndexedDB.retrieve[ModelStoreItem](IndexedDB.MODEL_STORE, model.id) flatMap { item =>
+      item.modelStore = default.write(model)
+
+      IndexedDB.update(IndexedDB.MODEL_STORE, item)
+    }
+  }
+
+  override def delete(model: ModelStore): Future[Unit] = {
+    IndexedDB.delete(IndexedDB.MODEL_STORE, model.id)
+  }
+
+  @js.native
+  trait ModelStoreItem extends js.Object {
+    var id: Double = js.native
+    var modelStore: String = js.native
+  }
+
+  object ModelStoreItem {
+
+    def extract(item: ModelStoreItem): ModelStore = {
+      default.read[ModelStore](item.modelStore)
+    }
+
+    def apply(id: Long, modelStore: String): ModelStoreItem = {
+      js.Dynamic.literal(
+        "id" -> id,
+        "modelStore" -> modelStore
+      ).asInstanceOf[ModelStoreItem]
+    }
+
+    def fromStore(modelStore: ModelStore): ModelStoreItem = {
+      apply(modelStore.id, default.write(modelStore))
+    }
   }
 }
