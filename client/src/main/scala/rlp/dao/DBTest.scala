4e3a0c8542f13e67855e15369960d183a9952160
Basim Khajwal <basimkhajwal@gmail.com>
2018-01-16 22:48:16 +0000

    Added a basic test (that works :D)

---
+++ client/src/main/scala/rlp/dao/DBTest.scala
@@ -0,0 +1,33 @@
+package rlp.dao
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+@JSExportTopLevel("DBTest")
+object DBTest {
+
+  import LocalModelDAO._
+  import IndexedDB._
+
+  @JSExport
+  def runTest(): Unit = {
+    for {
+      _ <- create(MODEL_STORE, js.Dynamic.literal("id" -> 1, "modelStore" -> "Lel, some model here..."))
+      _ <- create(MODEL_STORE, js.Dynamic.literal("id" -> 2, "modelStore" -> "Some other model here"))
+      _ <- create(MODEL_STORE, js.Dynamic.literal("id" -> 3, "modelStore" -> "Some other model here"))
+      _ <- create(MODEL_STORE, js.Dynamic.literal("id" -> 4, "modelStore" -> "Some other model here"))
+      _ <- create(MODEL_STORE, js.Dynamic.literal("id" -> 5, "modelStore" -> "Some other model here"))
+
+      _ <- update(MODEL_STORE, js.Dynamic.literal("id" -> 2, "modelStore" -> "CHANGED THE GUY!"))
+
+      _ <- delete(MODEL_STORE, 3)
+      _ <- delete(MODEL_STORE, 4)
+
+      items <- getAll[ModelStoreItem](MODEL_STORE)
+    } {
+      items foreach (x => println(x.id + " - " + x.modelStore))
+    }
+  }
+}
