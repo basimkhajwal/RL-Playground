6f77633c0b1c43cd6debbefa43d9592b8a19480d
Basim Khajwal <basimkhajwal@gmail.com>
2018-01-21 12:14:09 +0000

    Added server dao

--- client/src/main/scala/rlp/dao/ServerModelDAO.scala
+++ client/src/main/scala/rlp/dao/ServerModelDAO.scala
@@ -1,16 +1,36 @@
 package rlp.dao
 
+import org.scalajs.dom.ext.Ajax
 import rlp.storage.ModelStore
 
 import scala.concurrent.Future
+import upickle.default
+
+import scala.util.Try
 
 object ServerModelDAO extends ModelDAO {
 
-  override def getAll(): Future[Seq[ModelStore]] = ???
+  override def getAll(): Future[Seq[ModelStore]] = {
+    Ajax.get("/models") flatMap { request =>
+      Future.fromTry(Try {
+        default.read[Seq[ModelStore]](request.responseText)
+      })
+    }
+  }
 
-  override def create(model: ModelStore): Future[Long] = ???
+  override def create(model: ModelStore): Future[Long] = {
+    Ajax.put("/models", default.write(model)) flatMap { request =>
+      Future.fromTry(Try(
+        request.getResponseHeader("id").toLong
+      ))
+    }
+  }
 
-  override def update(model: ModelStore): Future[Unit] = ???
+  override def update(model: ModelStore): Future[Unit] = {
+    Ajax.post("/models", default.write(model)).map { _ => () }
+  }
 
-  override def delete(id: Long): Future[Unit] = ???
+  override def delete(id: Long): Future[Unit] = {
+    Ajax.delete("/models", headers = Map("id" -> id.toString)) map { _ => () }
+  }
 }
