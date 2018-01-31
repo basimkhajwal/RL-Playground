ee795145af48f9bed1d57ae9c1d5b494efc3b75a
Basim Khajwal <basimkhajwal@gmail.com>
2018-01-31 23:28:27 +0000

    Updated point2d

--- shared/src/main/scala/rlp/util/Point2D.scala
+++ shared/src/main/scala/rlp/util/Point2D.scala
@@ -1,15 +1,34 @@
 package rlp.util
 
 case class Point2D(x: Double, y: Double) {
+
   def +(that: Point2D) = Point2D(x + that.x, y + that.y)
+
   def -(that: Point2D) = Point2D(x - that.x, y - that.y)
+
   def *(scale: Double) = Point2D(x * scale, y * scale)
+
+  def /(scale: Double) = Point2D(x / scale, y / scale)
+
+  def norm(): Point2D = {
+    val m = magnitude()
+    if (m < 1e-10) this * 1e10 else this / m
+  }
+
   def magnitude(): Double = math.sqrt(x*x + y*y)
+
+  def distance(that: Point2D): Double = {
+    math.sqrt((x-that.x)*(x-that.x) + (y-that.y)*(y-that.y))
+  }
+
   def angle(): Double = {
     val a = math.atan2(y, x)
     if (a < 0) a+2*math.Pi else a
   }
 }
 object Point2D {
-  def fromPolar(theta: Double, m: Double): Point2D = Point2D(m*math.cos(theta), m*math.sin(theta))
+
+  def fromPolar(theta: Double, m: Double): Point2D = {
+    Point2D(m*math.cos(theta), m*math.sin(theta))
+  }
 }
