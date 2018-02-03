68879796bbc698c97177903e66a0bd36703daa27
basimkhajwal <basimkhajwal@gmail.com>
2018-02-03 16:22:10 +0000

    Added mean and standard deviation utility

---
+++ shared/src/main/scala/rlp/math/Distribution.scala
@@ -0,0 +1,28 @@
+package rlp.math
+
+object Distribution {
+
+  def mean(values: Array[Double]): Double = {
+    if (values.length > 0) {
+      values.sum / values.length
+    } else {
+      0
+    }
+  }
+
+  def stdDev(values: Array[Double]): Double = {
+
+    if (values.length < 2) return 0
+
+    val n = values.length
+
+    var sum = 0.0
+    var sumSquared = 0.0
+    for (value <- values) {
+      sum += value
+      sumSquared += value*value
+    }
+
+    math.sqrt((sumSquared - sum*sum/n) / (n-1))
+  }
+}
