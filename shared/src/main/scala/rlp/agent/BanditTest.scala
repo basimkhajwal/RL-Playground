371154c6b284248c7161d2967b3b22ee82f4eb5b
basimkhajwal <basimkhajwal@gmail.com>
2018-02-03 16:21:48 +0000

    Added bandit test for policy network

--- shared/src/main/scala/rlp/agent/BanditTest.scala
+++ shared/src/main/scala/rlp/agent/BanditTest.scala
@@ -9,14 +9,17 @@ object BanditTest {
   def main(args: Array[String]): Unit = {
 
     val bandits = Array(
-      -10000, 10, 12, -1
+      -10, 26, 12, 20
     )
 
     val qTable = new QTableAgent(1, bandits.length)
     val qNetwork = new QNetworkAgent(new NeuralNetwork(Array(1, 4, 4), Array(ReLU, Linear)), 1000)
+    val policyNetwork = new PolicyNetworkAgent(new NeuralNetwork(Array(1, 4), Array(Linear), true))
 
     qTable.discountFactor = 0
     qTable.explorationEpsilon = 0.5
+
+    policyNetwork.discountFactor = 0
 
     qNetwork.explorationEpsilon = 0.5
     qNetwork.discountFactor = 0
@@ -28,12 +31,20 @@ object BanditTest {
     for (_ <- 0 until 10000) {
       val r1 = bandits(qTable.act(0)) * (0.8 + math.random()*0.4)
       val r2 = bandits(qNetwork.act(Array(1.0))) * (0.8 + math.random()*0.4)
+      val r3 = bandits(policyNetwork.act(Array(1.0))) * (0.8 + math.random()*0.4)
+
       qTable.percept(r1)
       qNetwork.percept(r2)
+      policyNetwork.percept(r3)
+      policyNetwork.resetEpisode()
     }
 
     println(qTable.table mkString "\t")
+    println()
     println(qNetwork.network.weights(0).toString)
     println(qNetwork.network.forwardProp(Array(1.0)) mkString "\t")
+    println()
+    println(policyNetwork.network.weights(0).toString)
+    println(policyNetwork.network.forwardProp(Array(1.0)) mkString "\t")
   }
 }
