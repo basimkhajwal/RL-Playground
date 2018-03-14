ef15c83cc48b5e2a69a343a919f102de280de043
basimkhajwal <basimkhajwal@gmail.com>
2018-03-14 23:38:13 +0000

    Allowed listening to click events

--- client/src/main/scala/rlp/utils/KeyboardHandler.scala
+++ client/src/main/scala/rlp/utils/KeyboardHandler.scala
@@ -3,18 +3,31 @@ package rlp.utils
 import org.scalajs.dom.raw.{HTMLElement, KeyboardEvent}
 
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 class KeyboardHandler {
 
-  val captureKeys = List("ArrowUp", "ArrowDown")
+  val captureKeys = List("ArrowUp", "ArrowDown", " ")
 
   val keyMap: mutable.Map[String, Boolean] = mutable.Map()
+  val clickListeners: ListBuffer[String => _] = ListBuffer()
 
   def isKeyDown(key: String): Boolean = keyMap.getOrElse(key, false)
 
+  def registerClickListener(listener: String => _): Unit = {
+    clickListeners += listener
+  }
+
   def register(element: HTMLElement): Unit = {
     element.onkeydown = { e:KeyboardEvent => keyMap.put(e.key, true); if (captureKeys contains e.key) e.preventDefault() }
-    element.onkeyup = { e:KeyboardEvent => keyMap.put(e.key, false); if (captureKeys contains e.key) e.preventDefault() }
+    element.onkeyup = { e:KeyboardEvent =>
+      keyMap.put(e.key, false)
+      for (listener <- clickListeners) listener(e.key)
+
+      if (captureKeys contains e.key) {
+        e.preventDefault()
+      }
+    }
   }
 
 }
